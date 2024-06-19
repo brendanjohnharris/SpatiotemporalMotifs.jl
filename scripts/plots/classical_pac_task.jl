a1c2125be1406ab669c0d195dd3fdd595a04220e
brendanjohnharris <brendanjohnharris@gmail.com>
2024-06-19 18:18:30 +1000

    Checkpoint

--- scripts/plots/classical_pac_task.jl
+++ scripts/plots/classical_pac_task.jl
@@ -10,13 +10,12 @@ using FileIO
 using Unitful
 using ModulationIndices
 using Normalization
-import SpatiotemporalMotifs: plotdir, calcquality, layernum2name, savepath,
-                             structurecolors, structures, layercolors, tortinset!, @preamble
+using SpatiotemporalMotifs
 @preamble
 set_theme!(foresight(:physics))
 
 stimulus = r"Natural_Images"
-vars = [:ϕ, :aᵧ]
+vars = [:ϕ, :r]
 
 session_table = load(datadir("session_table.jld2"), "session_table")
 oursessions = session_table.ecephys_session_id
@@ -25,70 +24,72 @@ path = datadir("calculations")
 Q = calcquality(path)[structure = At(structures)]
 quality = mean(Q[stimulus = At(stimulus)])
 
-config = @strdict stimulus vars
-data, file = produce_or_load(config, datadir(); filename = savepath) do config
-    out = load_calculations(Q; path, stimulus, vars)
-    uni = unify_calculations(out; vars)
-    @strdict uni
-end
-uni = data["uni"]
-unidepths = getindex.(uni, :unidepths)
-layerints = getindex.(uni, :layerints)
-layernames = getindex.(uni, :layernames)
-layernums = getindex.(uni, :layernums)
-r = [abs.(uni[i][:aᵧ]) for i in eachindex(uni)]
-ϕ = [uni[i][:ϕ] for i in eachindex(uni)]
-uni = []
-GC.gc()
+begin
+    config = @strdict stimulus vars
+    data, file = produce_or_load(produce_uni, config, datadir(); filename = savepath)
+    uni = data["uni"]
+    unidepths = getindex.(uni, :unidepths)
+    layerints = getindex.(uni, :layerints)
+    layernames = getindex.(uni, :layernames)
+    layernums = getindex.(uni, :layernums)
+    r = [uni[i][:r] for i in eachindex(uni)]
+    ϕ = [uni[i][:ϕ] for i in eachindex(uni)]
+    ϕ = [mod2pi.(x .+ pi) .- pi for x in ϕ]
+    uni = []
+    GC.gc()
 
-ϕ_h = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== true] for i in eachindex(ϕ)]
-r_h = [r[i][:, :, lookup(r[i], :trial) .== true] for i in eachindex(r)]
-ϕ_m = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== false] for i in eachindex(ϕ)]
-r_m = [r[i][:, :, lookup(r[i], :trial) .== false] for i in eachindex(r)]
+    ϕ_h = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== true] for i in eachindex(ϕ)]
+    r_h = [r[i][:, :, lookup(r[i], :trial) .== true] for i in eachindex(r)]
+    ϕ_m = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== false] for i in eachindex(ϕ)]
+    r_m = [r[i][:, :, lookup(r[i], :trial) .== false] for i in eachindex(r)]
+end
 
 begin # * PAC over depths
-    function classicalpac(ϕ, r; kwargs...)
-        pac = dropdims(sum(ϕ, dims = (Ti, :trial)); dims = (Ti, :trial))
-        pac .= 0.0
-        mp = zip(eachslice(parent(ϕ); dims = 2), eachslice(parent(r); dims = 2))
-        Threads.@threads for (i, (_p, _a)) in collect(enumerate(mp))
-            pac[i] = ModulationIndices.tort2010(_p[:], _a[:]; kwargs...)
-        end
-        return pac
-    end
-
-    pac = classicalpac.(ϕ, r; n = 40)
-    pac_h = classicalpac.(ϕ_h, r_h; n = 40)
-    pac_m = classicalpac.(ϕ_m, r_m; n = 40)
+    pacc = [pac(ϕ[i], r[i]; dims = Ti) for i in eachindex(ϕ)]
+    pac_h = [pac(ϕ_h[i], r_h[i]; dims = Ti) for i in eachindex(ϕ_h)]
+    pac_m = [pac(ϕ_m[i], r_m[i]; dims = Ti) for i in eachindex(ϕ_m)]
 end
 
+begin # * Set up figure
+    f = FourPanel()
+    gs = subdivide(f, 2, 2)
+end
 begin # * Plot over layers
-    f = Figure(size = (560, 720))
-
-    ax = Axis(f[1, 1], limits = ((0, 100), (nothing, nothing)),
-              xlabel = "Cortical depth (%)", ylabel = "PAC", backgroundcolor = :transparent)
-    for i in eachindex(pac)
-        lines!(ax, 100 * lookup(pac[i], :depth), ustripall(pac[i]),
-               color = (structurecolors[i], 0.8), label = structures[i])
+    ax = Axis(gs[1][1, 1], limits = ((0, 100), (nothing, nothing)),
+              xlabel = "Cortical depth (%)", ylabel = "PAC", backgroundcolor = :transparent,
+              xtickformat = depthticks)
+    for i in eachindex(pacc)
+        s = structures[i]
+        μ, (σl, σh) = bootstrapmedian(pacc[i] |> ustripall; dims = 2)
+        mu = upsample(μ, 5)
+        l = upsample(σl, 5) |> parent
+        h = upsample(σh, 5) |> parent
+        band!(ax, lookup(mu, 1), l, h; color = (structurecolormap[s], 0.2),
+              label = s)
+        lines!(ax, lookup(mu, 1), mu, color = structurecolormap[s], label = s,
+               alpha = 0.8,
+               linewidth = 4)
+        scatter!(ax, lookup(μ, 1), μ, color = structurecolormap[s], label = s,
+                 markersize = 10, alpha = 0.8)
     end
-    axislegend(ax; position = :lt, nbanks = 2)
+    axislegend(ax; position = :lt, nbanks = 2, merge = true)
 
-    ax2 = Axis(f[2, 1], limits = ((0, 100), (nothing, nothing)),
-               xlabel = "Cortical depth (%)",
-               ylabel = "PAC difference (hit - miss)")
-    for i in eachindex(pac_m)
-        lines!(ax2, 100 * lookup(pac_m[i], :depth), ustripall(pac_h[i] .- pac_m[i]),
-               color = (structurecolors[i], 0.8), label = structures[i])
-    end
-    axislegend(ax2)
+    # ax2 = Axis(f[2, 1], limits = ((0, 100), (nothing, nothing)),
+    #            xlabel = "Cortical depth (%)",
+    #            ylabel = "PAC difference (hit - miss)")
+    # for i in eachindex(pac_m)
+    #     lines!(ax2, 100 * lookup(pac_m[i], :depth), ustripall(pac_h[i] .- pac_m[i]),
+    #            color = (structurecolors[i], 0.8), label = structures[i])
+    # end
+    # axislegend(ax2)
 
-    begin # * Add roseflowers
+    if true # * Add roseflowers
         idx = Dim{:depth}(Near(0.25))
         struc = 1
         tortinset!(f[1, 1], ϕ[struc][idx], r[struc][idx],
                    colormap = seethrough(structurecolors[struc], 0.1, 1),
                    halign = 0.3, valign = 0.6, color = structurecolors[struc])
-        scatter!(ax, [25], [pac[struc][idx]], color = structurecolors[struc],
+        scatter!(ax, [25], [pacc[struc][idx]], color = structurecolors[struc],
                  markersize = 15,
                  strokecolor = :white, strokewidth = 2)
 
@@ -97,7 +98,7 @@ begin # * Plot over layers
         tortinset!(f[1, 1], ϕ[struc][idx], r[struc][idx],
                    colormap = seethrough(structurecolors[struc], 0.1, 1),
                    halign = 0.01, valign = 0.4, color = structurecolors[struc])
-        scatter!(ax, [30], [pac[struc][idx]], color = structurecolors[struc],
+        scatter!(ax, [30], [pacc[struc][idx]], color = structurecolors[struc],
                  markersize = 15,
                  strokecolor = :white, strokewidth = 2)
 
@@ -106,7 +107,7 @@ begin # * Plot over layers
         tortinset!(f[1, 1], ϕ[struc][idx], r[struc][idx],
                    colormap = seethrough(structurecolors[struc], 0.1, 1),
                    halign = 0.87, valign = 0.95, color = structurecolors[struc])
-        scatter!(ax, [88], [pac[struc][idx]], color = structurecolors[struc],
+        scatter!(ax, [88], [pacc[struc][idx]], color = structurecolors[struc],
                  markersize = 15,
                  strokecolor = :white, strokewidth = 2)
 
@@ -115,61 +116,69 @@ begin # * Plot over layers
         tortinset!(f[1, 1], ϕ[struc][idx], r[struc][idx],
                    colormap = seethrough(structurecolors[struc], 0.1, 1),
                    halign = 0.9, valign = 0.48, color = structurecolors[struc])
-        scatter!(ax, [92], [pac[struc][idx]], color = structurecolors[struc],
+        scatter!(ax, [92], [pacc[struc][idx]], color = structurecolors[struc],
                  markersize = 15,
                  strokecolor = :white, strokewidth = 2)
     end
+end
 
+begin # * Polar plot of coupling angle peaks (find peaks? what if there is more than 1? )
+end
+
+begin # * LDA classification: performance and weightings
+end
+
+begin # * Save
     display(f)
     wsave(plotdir("classical_pac_task", "pac_mean.pdf"), f)
 end
 
-begin # * Single-trial PAC
-    function classicalpac(ϕ, r; kwargs...)
-        pac = dropdims(sum(ϕ, dims = (Ti,)); dims = (Ti))
-        pac .= 0.0
-        mp = zip(eachslice(parent(ϕ); dims = (2, 3)), eachslice(parent(r); dims = (2, 3)))
-        Threads.@threads for (i, (_p, _a)) in collect(enumerate(mp))
-            pac[i] = ModulationIndices.tort2010(_p[:], _a[:]; kwargs...)
-        end
-        return pac
-    end
+# begin # * Single-trial PAC
+#     function classicalpac(ϕ, r; kwargs...)
+#         pac = dropdims(sum(ϕ, dims = (Ti,)); dims = (Ti))
+#         pac .= 0.0
+#         mp = zip(eachslice(parent(ϕ); dims = (2, 3)), eachslice(parent(r); dims = (2, 3)))
+#         Threads.@threads for (i, (_p, _a)) in collect(enumerate(mp))
+#             pac[i] = ModulationIndices.tort2010(_p[:], _a[:]; kwargs...)
+#         end
+#         return pac
+#     end
 
-    pac = classicalpac.(ϕ, r; n = 20)
-    pac_h = [pac[i][:, lookup(pac[i], :trial) .== true] for i in eachindex(pac)]
-    pac_m = [pac[i][:, lookup(pac[i], :trial) .== false] for i in eachindex(pac)]
-end
-begin
-    f = Figure(size = (560, 720))
-    ax = Axis(f[1, 1], limits = ((0, 100), (nothing, nothing)),
-              xlabel = "Cortical depth (%)", ylabel = "Mean PAC")
-    for i in eachindex(pac)
-        μ = dropdims(mean(pac[i], dims = :trial), dims = :trial) |> ustripall
-        σ = dropdims(std(pac[i], dims = :trial), dims = :trial) |> ustripall
-        σ = σ ./ sqrt(size(pac[i], 2)) # SEM
+#     pacc = classicalpac.(ϕ, r; n = 20)
+#     pac_h = [pacc[i][:, lookup(pac[i], :trial) .== true] for i in eachindex(pacc)]
+#     pac_m = [pacc[i][:, lookup(pac[i], :trial) .== false] for i in eachindex(pacc)]
+# end
+# begin
+#     f = Figure(size = (560, 720))
+#     ax = Axis(f[1, 1], limits = ((0, 100), (nothing, nothing)),
+#               xlabel = "Cortical depth (%)", ylabel = "Mean PAC")
+#     for i in eachindex(pac)
+#         μ = dropdims(mean(pac[i], dims = :trial), dims = :trial) |> ustripall
+#         σ = dropdims(std(pacc[i], dims = :trial), dims = :trial) |> ustripall
+#         σ = σ ./ sqrt(size(pacc[i], 2)) # SEM
 
-        band!(ax, 100 * lookup(pac[i], :depth), collect(μ .- σ),
-              collect(μ .+ σ);
-              color = (structurecolors[i], 0.3))
-        lines!(ax, 100 * lookup(pac[i], :depth), μ,
-               color = (structurecolors[i], 0.8), label = structures[i])
-    end
-    axislegend(ax; position = :ct, nbanks = 2)
+#         band!(ax, 100 * lookup(pacc[i], :depth), collect(μ .- σ),
+#               collect(μ .+ σ);
+#               color = (structurecolors[i], 0.3))
+#         lines!(ax, 100 * lookup(pacc[i], :depth), μ,
+#                color = (structurecolors[i], 0.8), label = structures[i])
+#     end
+#     axislegend(ax; position = :ct, nbanks = 2)
 
-    ax = Axis(f[2, 1], limits = ((0, 100), (nothing, nothing)),
-              xlabel = "Cortical depth (%)",
-              ylabel = "PAC difference (Cohen's d)")
-    for i in eachindex(pac)
-        μ_h = dropdims(mean(pac_h[i], dims = :trial), dims = :trial) |> ustripall
-        μ_m = dropdims(mean(pac_m[i], dims = :trial), dims = :trial) |> ustripall
-        σ = dropdims(std(pac[i], dims = :trial), dims = :trial) |> ustripall
-        d = (μ_h .- μ_m) ./ σ # * Cohen's d effect size
+#     ax = Axis(f[2, 1], limits = ((0, 100), (nothing, nothing)),
+#               xlabel = "Cortical depth (%)",
+#               ylabel = "PAC difference (Cohen's d)")
+#     for i in eachindex(pacc)
+#         μ_h = dropdims(mean(pac_h[i], dims = :trial), dims = :trial) |> ustripall
+#         μ_m = dropdims(mean(pac_m[i], dims = :trial), dims = :trial) |> ustripall
+#         σ = dropdims(std(pacc[i], dims = :trial), dims = :trial) |> ustripall
+#         d = (μ_h .- μ_m) ./ σ # * Cohen's d effect size
 
-        lines!(ax, 100 * lookup(d, :depth), d,
-               color = (structurecolors[i], 0.8), label = structures[i])
-    end
-    axislegend(ax; position = :rt, nbanks = 2)
+#         lines!(ax, 100 * lookup(d, :depth), d,
+#                color = (structurecolors[i], 0.8), label = structures[i])
+#     end
+#     axislegend(ax; position = :rt, nbanks = 2)
 
-    display(f)
-    wsave(plotdir("classical_pac_task", "pac_singletrial.pdf"), f)
-end
+#     display(f)
+#     wsave(plotdir("classical_pac_task", "pac_singletrial.pdf"), f)
+# end
--- scripts/plots/pac_task.jl
+++ scripts/plots/pac_task.jl
@@ -24,6 +24,78 @@ Q = calcquality(path)[structure = At(structures)]
 Q = Q[sessionid = At(oursessions)]
 quality = mean(Q[stimulus = At(stimulus)])
 
+begin # * Set up main figure
+    mf = TwoPanel()
+    mgs = subdivide(mf, 1, 2)
+end
+
+stimuli = ["r\"Natural_Images\"", "spontaneous", "flash_250ms"]
+pQ = calcquality(datadir("power_spectra"))
+for stimulus in stimuli
+    _Q = pQ[stimulus = At(stimulus), structure = At(structures)]
+    subsessions = intersect(oursessions, lookup(_Q, :sessionid))
+    if length(subsessions) < length(oursessions)
+        @warn "Power spectra calculations are incomplete, proceeding regardless"
+    end
+    _Q = _Q[Dim{:sessionid}(At(subsessions))]
+    filebase = stimulus == "spontaneous" ? "" : "_$stimulus"
+    f = Figure(size = (900, 1080))
+
+    begin # * Load data
+        S = map(lookup(_Q, :structure)) do structure
+            out = map(lookup(_Q, :sessionid)) do sessionid
+                if _Q[sessionid = At(sessionid), structure = At(structure)] == 0
+                    return nothing
+                end
+                filename = savepath((@strdict sessionid structure stimulus), "jld2",
+                                    datadir("power_spectra"))
+                C = load(filename, "C")
+                # S = load(filename, "sC")
+                # return (C .- S) ./ median(S)
+            end
+            idxs = .!isnothing.(out)
+            out = out[idxs]
+
+            m = DimensionalData.metadata.(out)
+            out = map(out) do o
+                dropdims(mean(o, dims = :channel), dims = :channel)
+            end
+            out = stack(Dim{:sessionid}(lookup(_Q, :sessionid)[idxs]), out, dims = 3)
+            return out
+        end
+    end
+
+    begin # * Supplemental average comodulograms
+        f = SixPanel()
+        gs = subdivide(f, 3, 2)
+        map(gs, structures, S) do g, structure, s
+            ax = Axis(g[1, 1]; title = structure, xlabel = "Phase frequency (Hz)",
+                      ylabel = "Amplitude frequency (Hz)")
+            s = dropdims(mean(s, dims = :sessionid); dims = :sessionid)
+            s = upsample(s, 5, 1)
+            s = upsample(s, 5, 2)
+            p = heatmap!(ax, s; colormap = seethrough(reverse(sunrise)), rasterize = 5)
+            Colorbar(g[1, 2], p; label = "Modulation index")
+        end
+        addlabels!(f)
+        display(f)
+        wsave(plotdir("comodulograms", "comodulograms_$stimulus.pdf"), f)
+    end
+
+    if stimulus == "spontaneous" # * Plot into main figure
+        structure = "VISl"
+        ax = Axis(mgs[1][1, 1]; title = structure, xlabel = "Phase frequency (Hz)",
+                  ylabel = "Amplitude frequency (Hz)")
+        s = S[lookup(_Q, :structure) .== structure] |> only
+        display(s)
+        s = dropdims(mean(s, dims = :sessionid); dims = :sessionid)
+        s = upsample(s, 5, 1)
+        s = upsample(s, 5, 2)
+        p = heatmap!(ax, s; colormap = seethrough(reverse(sunrise)), rasterize = 5)
+        Colorbar(mgs[1][1, 2], p; label = "Mean PAC")
+    end
+end
+
 config = @strdict stimulus vars
 data, file = produce_or_load(produce_uni, config, datadir(); filename = savepath)
 uni = data["uni"]
@@ -39,6 +111,7 @@ begin # * Normalize amplitudes and generate a burst mask
     r_m = [r[i][:, :, lookup(r[i], :trial) .== false] for i in eachindex(r)]
 
     ϕ = [uni[i][:ϕ] for i in eachindex(uni)]
+    ϕ = [mod2pi.(x .+ pi) .- pi for x in ϕ]
     ϕ_h = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== true] for i in eachindex(ϕ)]
     ϕ_m = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== false] for i in eachindex(ϕ)]
 
@@ -67,111 +140,125 @@ begin # * Supplemental figure: spatiotemporal PAC over all regions
         ax = Axis(g[1, 1]; title = s, yreversed = true,
                   limits = (nothing, (extrema(lookup(P, :depth)))))
         p = plotlayermap!(ax, ustripall(P[Ti = SpatiotemporalMotifs.INTERVAL]), l) |> first
-        Colorbar(g[1, 2], p)
+        Colorbar(g[1, 2], p, label = "PAC")
+
+        if s == "VISl"
+            ax = Axis(mgs[2][1, 1]; title = s, yreversed = true,
+                      limits = (nothing, (extrema(lookup(P, :depth)))))
+            p = plotlayermap!(ax, ustripall(P[Ti = SpatiotemporalMotifs.INTERVAL]), l) |>
+                first
+            Colorbar(mgs[2][1, 2], p, label = "PAC")
+        end
     end
+    addlabels!(f)
     f
 end
 
-begin
-    mask_h = [r̂_h[i] .> 2.0 for i in eachindex(r̂_h)]
-    mask_m = [r̂_m[i] .> 2.0 for i in eachindex(r̂_m)]
-
-    ϕ = [abs.(uni[i][:ϕ]) for i in eachindex(uni)]
-
-    ϕ_h = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== true] for i in eachindex(ϕ)]
-    ϕ̂_h = deepcopy(ϕ_h)
-    [ϕ̂_h[i][.!mask_h[i]] .= NaN for i in eachindex(mask_h)]
-
-    ϕ_m = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== false] for i in eachindex(ϕ)]
-    ϕ̂_m = deepcopy(ϕ_m)
-    [ϕ̂_m[i][.!mask_m[i]] .= NaN for i in eachindex(mask_m)]
-
-    ϕ = []
-    uni = []
-    GC.gc()
+begin # * Save figure
+    addlabels!(mf)
+    wsave(plotdir("pac_task", "average_pac.pdf"), mf)
 end
 
-begin # * Heatmap of the number of samples
-    pn_h = dropdims.(sum.(mask_h, dims = 3), dims = 3)
-    pn_m = dropdims.(sum.(mask_m, dims = 3), dims = 3)
+# begin
+#     mask_h = [r̂_h[i] .> 2.0 for i in eachindex(r̂_h)]
+#     mask_m = [r̂_m[i] .> 2.0 for i in eachindex(r̂_m)]
 
-    f = Figure(size = (720, 1440))
+#     ϕ = [abs.(uni[i][:ϕ]) for i in eachindex(uni)]
 
-    for i in eachindex(pn_h)
-        # * Hit
-        ax = Axis(f[i, 1], yreversed = true)
-        structure = DimensionalData.metadata(pn_h[i])[:structure]
-        ax.title = structure * ": hit"
-        m = ustripall(pn_h[i])
-        colorrange = (nmin, max(maximum(m), maximum(ustripall(pn_m[i]))))
-        ints = layerints[i]
-        p = plotlayermap!(ax, m, ints; arrows = false, colorrange,
-                          colormap = :bone, lowclip = :crimson) |>
-            first
-        if i > 5
-            ax.xlabel = "Time (s)"
-        end
+#     ϕ_h = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== true] for i in eachindex(ϕ)]
+#     ϕ̂_h = deepcopy(ϕ_h)
+#     [ϕ̂_h[i][.!mask_h[i]] .= NaN for i in eachindex(mask_h)]
 
-        # * Miss
-        ax = Axis(f[i, 2], yreversed = true)
-        ax.title = structure * ": miss"
-        m = ustripall(pn_m[i])
-        p = plotlayermap!(ax, m, ints; arrows = false, colorrange,
-                          colormap = :bone, lowclip = :crimson) |>
-            first
-        c = Colorbar(f[i, 3], p)
-        c.label = "Num. of bursts"
-    end
+#     ϕ_m = [ϕ[i][:, :, lookup(ϕ[i], :trial) .== false] for i in eachindex(ϕ)]
+#     ϕ̂_m = deepcopy(ϕ_m)
+#     [ϕ̂_m[i][.!mask_m[i]] .= NaN for i in eachindex(mask_m)]
 
-    wsave(plotdir("pac_task", "supplemental_numsamples.pdf"), f)
-end
+#     ϕ = []
+#     uni = []
+#     GC.gc()
+# end
 
-begin # * Heatmap of the PAC entropy
-    function pacentropy(ϕ) # ?! Normalised or unnormalized? Domain is always the same, so...?
-        _p = dropdims(nansafe(x -> StateSpaceSet(x); dims = 3)(ϕ), dims = 3)
-        function ent(x)
-            if isempty(x)
-                return 3 # Some entropy higher than the maximum possible, 1
-            else
-                ComplexityMeasures.information_normalized(Shannon(),
-                                                          ValueBinning(RectangularBinning(20)),
-                                                          StateSpaceSet(x))
-            end
-        end
-        return ent.(_p)
-    end
+# begin # * Heatmap of the number of samples
+#     pn_h = dropdims.(sum.(mask_h, dims = 3), dims = 3)
+#     pn_m = dropdims.(sum.(mask_m, dims = 3), dims = 3)
 
-    PAC_h = pacentropy.(ϕ̂_h)
-    PAC_m = pacentropy.(ϕ̂_m)
+#     f = Figure(size = (720, 1440))
 
-    f = Figure(size = (720, 1440))
+#     for i in eachindex(pn_h)
+#         # * Hit
+#         ax = Axis(f[i, 1], yreversed = true)
+#         structure = DimensionalData.metadata(pn_h[i])[:structure]
+#         ax.title = structure * ": hit"
+#         m = ustripall(pn_h[i])
+#         colorrange = (nmin, max(maximum(m), maximum(ustripall(pn_m[i]))))
+#         ints = layerints[i]
+#         p = plotlayermap!(ax, m, ints; arrows = false, colorrange,
+#                           colormap = :bone, lowclip = :crimson) |>
+#             first
+#         if i > 5
+#             ax.xlabel = "Time (s)"
+#         end
 
-    for i in eachindex(PAC_h)
-        # * Hit
-        ax = Axis(f[i, 1], yreversed = true)
-        structure = DimensionalData.metadata(pn_h[i])[:structure]
-        ax.title = structure * ": hit"
-        m = ustripall(PAC_h[i])
-        colorrange = [extrema(m)..., extrema(ustripall(PAC_m[i]))...] |> extrema
-        m[pn_h[i] .< nmin] .= 2
-        ints = layerints[i]
-        p = plotlayermap!(ax, m, ints; arrows = false, colormap = :bone, colorrange,
-                          highclip = :crimson) |>
-            first
-        if i > 5
-            ax.xlabel = "Time (s)"
-        end
+#         # * Miss
+#         ax = Axis(f[i, 2], yreversed = true)
+#         ax.title = structure * ": miss"
+#         m = ustripall(pn_m[i])
+#         p = plotlayermap!(ax, m, ints; arrows = false, colorrange,
+#                           colormap = :bone, lowclip = :crimson) |>
+#             first
+#         c = Colorbar(f[i, 3], p)
+#         c.label = "Num. of bursts"
+#     end
 
-        # * Miss
-        ax = Axis(f[i, 2], yreversed = true)
-        ax.title = structure * ": miss"
-        m = ustripall(PAC_m[i])
-        m[pn_m[i] .< nmin] .= 2
-        p = plotlayermap!(ax, m, ints; arrows = false, colorrange, highclip = :crimson,
-                          colormap = :bone) |>
-            first
-        c = Colorbar(f[i, 3], p)
-        c.label = "PAC entropy"
-    end
-    wsave(plotdir("pac_task", "supplemental_PAC.pdf"), f)
-end
+#     wsave(plotdir("pac_task", "supplemental_numsamples.pdf"), f)
+# end
+
+# begin # * Heatmap of the PAC entropy
+#     function pacentropy(ϕ) # ?! Normalised or unnormalized? Domain is always the same, so...?
+#         _p = dropdims(nansafe(x -> StateSpaceSet(x); dims = 3)(ϕ), dims = 3)
+#         function ent(x)
+#             if isempty(x)
+#                 return 3 # Some entropy higher than the maximum possible, 1
+#             else
+#                 ComplexityMeasures.information_normalized(Shannon(),
+#                                                           ValueBinning(RectangularBinning(20)),
+#                                                           StateSpaceSet(x))
+#             end
+#         end
+#         return ent.(_p)
+#     end
+
+#     PAC_h = pacentropy.(ϕ̂_h)
+#     PAC_m = pacentropy.(ϕ̂_m)
+
+#     f = Figure(size = (720, 1440))
+
+#     for i in eachindex(PAC_h)
+#         # * Hit
+#         ax = Axis(f[i, 1], yreversed = true)
+#         structure = DimensionalData.metadata(pn_h[i])[:structure]
+#         ax.title = structure * ": hit"
+#         m = ustripall(PAC_h[i])
+#         colorrange = [extrema(m)..., extrema(ustripall(PAC_m[i]))...] |> extrema
+#         m[pn_h[i] .< nmin] .= 2
+#         ints = layerints[i]
+#         p = plotlayermap!(ax, m, ints; arrows = false, colormap = :bone, colorrange,
+#                           highclip = :crimson) |>
+#             first
+#         if i > 5
+#             ax.xlabel = "Time (s)"
+#         end
+
+#         # * Miss
+#         ax = Axis(f[i, 2], yreversed = true)
+#         ax.title = structure * ": miss"
+#         m = ustripall(PAC_m[i])
+#         m[pn_m[i] .< nmin] .= 2
+#         p = plotlayermap!(ax, m, ints; arrows = false, colorrange, highclip = :crimson,
+#                           colormap = :bone) |>
+#             first
+#         c = Colorbar(f[i, 3], p)
+#         c.label = "PAC entropy"
+#     end
+#     wsave(plotdir("pac_task", "supplemental_PAC.pdf"), f)
+# end
