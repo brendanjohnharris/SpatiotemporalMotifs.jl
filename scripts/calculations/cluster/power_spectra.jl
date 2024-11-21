7a62498d493c5f7a9447fe3f0ee12480044e3e75
brendanjohnharris <brendanjohnharris@gmail.com>
2024-11-21 17:52:10 +1100

    Up

--- scripts/calculations/cluster/power_spectra.jl
+++ scripts/calculations/cluster/power_spectra.jl
@@ -39,6 +39,7 @@ if haskey(ENV, "JULIA_DISTRIBUTED")
                                  retry_errors = $retry_errors)
         end
     end
+    nn = length(exprs) ÷ 2
     USydClusters.Physics.runscripts(exprs[1:nn]; ncpus = 16, mem = 100, walltime = 1,
                                     project = projectdir(), qsub_flags = "-q yossarian")
     USydClusters.Physics.runscripts(exprs[(nn + 1):end]; ncpus = 16, mem = 100,
